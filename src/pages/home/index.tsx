621323904db448a79810cd8c9dffae7774bd8841
Nikhil Joshi <nikhilakjoshi@gmail.com>
2023-11-01 23:13:53 -0500

    fix

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,28 @@
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+
 export default function Home() {
+  const router = useRouter();
+  const [showAccept, setShowAccept] = useState<boolean>(false);
+  useEffect(() => {
+    document.hasStorageAccess().then((a) => {
+      if (!a) setShowAccept(true);
+    });
+  }, []);
   return (
     <>
       <h1>Child Home</h1>
+      {showAccept && (
+        <button
+          onClick={() => {
+            document.requestStorageAccess().then(() => {
+              router.replace("/setcookie");
+            });
+          }}
+        >
+          Accept Cookies
+        </button>
+      )}{" "}
     </>
   );
 }
